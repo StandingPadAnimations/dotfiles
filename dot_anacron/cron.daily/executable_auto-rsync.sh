330b88731e5da3856a70a93ec7836c2193a681eb
mahid <mahidsheikh@pm.me>
2023-04-02 00:30:03 -0500

    Updated rsync script to use pass

--- dot_anacron/cron.daily/executable_auto-rsync.sh
+++ dot_anacron/cron.daily/executable_auto-rsync.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 CURRENT_DIR="$(dirname "$(which "$0")")"
-sshpass -f ~/.secrets rsync -avzP --delete --delete-excluded --exclude-from="$CURRENT_DIR/rsync-homedir-excludes.txt" /home mahid@192.168.1.6::NetBackup
+pass show Remote/rsync | sshpass rsync -avzP --delete --delete-excluded --exclude-from="$CURRENT_DIR/rsync-homedir-excludes.txt" /home mahid@192.168.1.6::NetBackup
