9db3710757cb7aaf281c94f56c489081b580b724
mahid <mahidsheikh@pm.me>
2023-04-23 18:17:59 -0500

    Updated Hyprland and Waybar configs

--- dot_config/hypr/wallpaper/wallpaper.sh
+++ dot_config/hypr/wallpaper/wallpaper.sh
@@ -7,10 +7,25 @@ export SWWW_TRANSITION_STEP=20
 # This controls (in seconds) when to switch to the next image
 INTERVAL=5
 
+# All positions
+POSITIONS=("top-right" "top-left" "bottom-right" "bottom-left")
+
+# Lock file
 exec 100>"$HOME/.config/hypr/wallpaper/animated-wallpaper.lock" || exit 1
 flock -w 10 100 || exit 1
+
+img="$(find "$HOME"/.config/hypr/wallpaper/{*.png,*.jpg} | shuf | head -1)"
 while true; do
-	img="$(find "$HOME"/.config/hypr/wallpaper/*.png | shuf | head -1)"
-	swww img "$img" --transition-type outer --transition-pos top-right
+	pos="${POSITIONS[$RANDOM % ${#POSITIONS[@]}]}"
+	while true; do
+		n_img="$(find "$HOME"/.config/hypr/wallpaper/{*.png,*.jpg} | shuf | head -1)"
+		if [ "$n_img" != "$img" ]
+		then
+			img="$n_img"
+			break
+		fi
+	done
+
+	swww img "$img" --transition-type outer --transition-pos "$pos"
 	sleep $INTERVAL
 done
